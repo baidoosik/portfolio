ea7762f6d655e59f76a72e934725751ea50eabde
doosik <qoentlr37@naver.com>
2017-04-01 00:33:06 +0900

    main.html 파일 옮기고 설정값 변경, staticfiles_dir, static_root 설정, main page 뷰 설정

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIR = os.path.join(BASE_DIR,'mysite','static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL ='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns =[
-    #url(r'^$',views.post_list, name='post_list')
+    url(r'^$',views.main_view,name='main_view')
 ]
 
 
