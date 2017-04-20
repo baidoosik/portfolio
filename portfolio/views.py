11e6421cd9109f925886fb797346b07cbae93f95
doosik <qoentlr37@naver.com>
2017-04-20 21:14:43 +0900

    views.py resume 부분 완성

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import os
+from django.shortcuts import redirect, render,get_object_or_404
+from django.http import HttpResponse,JsonResponse
+
 
 
 # Create your views here.
@@ -17,7 +20,17 @@ def project_view(request):
 def experience_view(request):
     return render(request,'portfolio/home.html')
 
-def resume_view(request):
+def resume_view(request):# 이력서 다운로드.
+    # 엑셀 다운로드 응답하기
+    filepath = '/Users/doosikbai/worldnomade/portfolio/static/portfolio/assets/resume.pages'
+    filename = os.path.basename(filepath)
+
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.pages')
+        # 필요한 응답헤더 세팅
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
+
     return render(request,'portfolio/home.html')
 
 def profile_view(request):
