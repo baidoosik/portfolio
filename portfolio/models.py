4acb845ad4af5b63e845bb4fddfbf17be48b99e3
doosik <qoentlr37@naver.com>
2017-04-25 23:59:08 +0900

    models.py에 post 모델추가

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,7 @@
 #portfolio/models.py
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 # Create your models here.
 class TimestampModel(models.Model):
@@ -10,5 +12,24 @@ class TimestampModel(models.Model):
         abstract = True
 
 
+class Post(TimestampModel):
+    title = models.CharField(max_length=100)
+    category= models.CharField(max_length=100,
+        choices = (
+            ('Electronic-Eng','Electronic-Eng'),
+            ('Start-up','Start-up'),
+            ('Project','Project'),
+            ('Experience','Experience'),
+        ),default=False)
 
+    photo1 = ProcessedImageField(blank=True, upload_to='blog/photo1/%Y/%M/%D',
+                                processors=[Thumbnail(500,500)],
+                                format='JPEG',
+                                options={'quality':60})
 
+    photo2 = ProcessedImageField(blank=True, upload_to='blog/photo2/%Y/%M/%D',
+                                processors=[Thumbnail(500,500)],
+                                format='JPEG',
+                                options={'quality':60})
+
+    summary = models.CharField(max_length=200)
